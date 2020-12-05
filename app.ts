565ab6e569a6a3ba846485ad7526d88c45e9c72d
damr <damr18@gmail.com>
2020-12-05 12:45:29 -0600

    firebase and show local notification

--- app.ts
+++ app.ts
@@ -5,27 +5,47 @@ purpose of the file is to pass control to the app’s first module.
 */
 
 import { Application } from '@nativescript/core';
-import { firebase } from "@nativescript/firebase";
+import { LocalNotifications } from "nativescript-local-notifications";
+// import { firebase } from "@nativescript/firebase";
 
-firebase.init({
-  onPushTokenReceivedCallback: function(token) {
-    console.log("Firebase push token: " + token);
-  },
-  showNotificationsWhenInForeground: true,
-  onMessageReceivedCallback: function(message) {
-    console.log("Title: " + message.title);
-    console.log("Body: " + message.body);
-    // if your server passed a custom property called 'foo', then do this:
-    console.log("Value of 'foo': " + message.data.foo);
-  },
-  }).then(
-    () => {
-      console.log("firebase.init done");
+// firebase.init({
+//   onPushTokenReceivedCallback: function(token) {
+//     console.log("Firebase push token: " + token);
+//   },
+//   showNotificationsWhenInForeground: true,
+//   onMessageReceivedCallback: function(message) {
+//     console.log("Title: " + message.title);
+//     console.log("Body: " + message.body);
+//     // if your server passed a custom property called 'foo', then do this:
+//     console.log("Value of 'foo': " + message.data.foo);
+//   },
+//   }).then(
+//     () => {
+//       console.log("firebase.init done");
+//     },
+//     error => {
+//       console.log(`firebase.init error: ${error}`);
+//     }
+//   );
+//   firebase.subscribeToTopic("lugares").then(() => console.log("Subscribed to topic"));
+
+
+LocalNotifications.schedule([{
+  id: 1,
+  title: 'The first title',
+  body: 'The first  body',
+  ticker: 'The ticker',
+  badge: 1,
+  sound: "sound1", //sound1 from /Appresources/raw/ folder
+  at: new Date(new Date().getTime() + (5 * 1000)) 
+}]).then(
+    function() {
+      console.log("Notification scheduled 1");
     },
-    error => {
-      console.log(`firebase.init error: ${error}`);
+    function(error) {
+      console.log("scheduling error: " + error);
     }
-  );
+);
 
 Application.run({ moduleName: 'app-root' });
 
