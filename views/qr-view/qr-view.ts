a4e1759a7a745c416fb7cc34f03226073bcff7b9
damr <damr18@gmail.com>
2020-12-07 21:19:03 -0600

    changes

--- views/qr-view/qr-view.ts
+++ views/qr-view/qr-view.ts
@@ -11,50 +11,15 @@ var page
 var userCode
 
 exports.onPageLoaded = function(args){
-  BackgroundFetch.configure({
-    minimumFetchInterval: 1,
-    stopOnTerminate: false,
-    startOnBoot: true,
-    enableHeadless: false,
-    
-}, () => {
-
-    LocalNotifications.schedule([{
-        id: 1,
-        title: 'The first title',
-        body: 'The first  body',
-        ticker: 'The ticker',
-        badge: 1,
-        sound: "sound1", //sound1 from /Appresources/raw/ folder
-        at: new Date(new Date().getTime() + (1 * 1000)) 
-      }]).then(
-          function() {
-            console.log("Notification scheduled 1");
-          },
-          function(error) {
-            console.log("scheduling error: " + error);
-          }
-      )
-    console.log("[BackgroundFetch] Event Received!");
   
-    // BackgroundFetch.finish();
-}, (error) => {
-    console.log("[BackgroundFetch] FAILED");
-    console.log(error);
-});
-  
-BackgroundFetch.start(() => {
-  
-    console.log("BackgroundFetch successfully started");
-}, (status) => {
-    console.log("BackgroundFetch failed to start: ", status);
-});
   
    
     page = args.object
     
     
     userCode = appSettings.getString("userCode","vacio");
+
+
 
     // verificar que existe la variable codeUser
     if(userCode==='vacio'){
@@ -74,7 +39,76 @@ BackgroundFetch.start(() => {
     }else{
       // obtener el valor de codeUser y generar QR
       generarQR(userCode)
+
     }
+    //aqui poner el buscar afluencia
+    obtenerAfluencia().then((afluMax) =>{
+      BackgroundFetch.finish();
+      BackgroundFetch.configure({
+        minimumFetchInterval: 1,
+        stopOnTerminate: false,
+        startOnBoot: true,
+        enableHeadless: false,
+        
+    }, () => {
+      if(afluMax){
+        LocalNotifications.schedule([{
+          id: 1,
+          title: 'SCAEI - Alerta de afluencia',
+          body: 'Aforo maximo ha sido superado',
+          ticker: 'The ticker',
+          badge: 1,
+          sound: "sound1", //sound1 from /Appresources/raw/ folder
+          at: new Date(new Date().getTime() + (1 * 1000)) 
+        }]).then(
+            function() {
+              console.log("Notification scheduled 1");
+            },
+            function(error) {
+              console.log("scheduling error: " + error);
+            }
+        )
+       }else{
+        LocalNotifications.schedule([{
+          id: 1,
+          title: 'SCAEI',
+          body: 'Aforo bajo el limite permitido',
+          ticker: 'The ticker',
+          badge: 1,
+          sound: "sound1", //sound1 from /Appresources/raw/ folder
+          at: new Date(new Date().getTime() + (1 * 1000)) 
+        }]).then(
+            function() {
+              console.log("Notification scheduled 1");
+            },
+            function(error) {
+              console.log("scheduling error: " + error);
+            }
+        )
+  
+       }
+       
+        console.log("[BackgroundFetch] Event Received!");
+      
+        // BackgroundFetch.finish();
+    }, (error) => {
+        console.log("[BackgroundFetch] FAILED");
+        console.log(error);
+    });
+      
+    BackgroundFetch.start(() => {
+      
+        console.log("BackgroundFetch successfully started");
+    }, (status) => {
+        console.log("BackgroundFetch failed to start: ", status);
+    });
+      
+
+     
+
+    }).catch((e) =>{
+      console.log(e)
+    }) 
      
 }
 
@@ -90,6 +124,40 @@ function generarQR(codigoStr){
   }
 }
 
+
+function obtenerAfluencia(){
+
+  return new Promise((resolve,reject) => {
+    
+    httpModule.request({
+      
+      url: "https://www.covidcinvestav.com/index.php?r=api/concurrenciavisita",
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      content: JSON.stringify({
+        "Individuo":
+        {
+          "codigo":appSettings.getString("userCode","vacio")
+        },
+        "LoginForm":
+        {
+          "username":"negocio",
+          "password":"jvW13%b2020"
+        }
+      })
+      }).then(response => {
+        return response.content.toJSON()
+
+      }).then(data => {
+        
+        resolve(data);
+
+      }).catch((e) => {
+          reject(e);
+      })  
+  });
+  
+}
 
 function obtenerCodigo(){
 
